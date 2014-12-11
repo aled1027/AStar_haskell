e212539b0015d309ba280bbe7f261aabee6507d9
aled1027 <a.led1027@gmail.com>
2014-12-11 15:41:03 -0800

    Looks a lot cleaner. Added when statements

--- Main.hs
+++ Main.hs
@@ -12,10 +12,14 @@ import Control.Monad
 import Control.Monad.Trans
 
 {-
+ - https://downloads.haskell.org/~ghc/latest/docs/html/libraries/containers/Data-Map-Lazy.html
+ -  - docs on Data.Map are for some reason here and not at Data.Map
+://downloads.haskell.org/~ghc/latest/docs/html/libraries/containers/Data-Map-Lazy.html
  - Make sure that it finds shortest path with a simple, 3x3 map of all Emptys. 
  -
  - change some if statements to when
  -
+ - Add world to getNeighbors to make sure the neighbors are acceptable
  -
  - Add map aspect to it, need to check the map to make sure that a point is viable. 
  -  - could add 
@@ -42,70 +46,79 @@ type SearchState = ([Pos],[Pos],Map.Map Pos Int) -- (openset, closedset, gScoreM
 
 -- Input Settings
 worldSize = 10 
-startSearchState = ([(1,2)],[],startMap)  -- starts with first node on it
+startSearchState = ([(2,2)],[],startMap)  -- starts with first node on it
 
 startMap = Map.fromList $ map (\x -> (x, startValue)) allPoints
     where allPoints = (,) <$> [1..worldSize] <*> [1..worldSize]
-          startValue = worldSize * worldSize
+          startValue = 0
 
-getNeighbors p@(x,y) = 
+getNeighbors p = [(1,1)]
+getNeighbors' p@(x,y) = 
     ((,) <$> xs <*> ys) DL.\\ [p]
     where delta = [-1, 0, 1]
           xs = map (+x) delta
           ys = map (+y) delta
 
 exit = left
-goal = (-10,-30)
-heuristicDist u v = abs (fst u - fst v) + abs (snd u - snd v)
+goal = (10,10)
 
---go :: State SearchState Int
-go = do
+heuristicDist u v = max dx dy
+    where dx = abs (fst u - fst v)
+          dy = abs (snd u - snd v)
+
+go :: [[Cell]] -> State SearchState String
+go world = do
   -- we always assume that our Maps will work, since they are preloaded 
   (current:a, b, c) <- get
   -- remove current from openlist
   modify (\(a,b,c) -> (tail a,b,c))
+
+  -- loop over the neighbors
   runEitherT $ forM (getNeighbors current) 
     (\neighbor ->  do
-      modify (\(a,b,c) -> (a,neighbor:b,c))
+
+      modify (\(a,b,c) -> (a,current:b,c))
       (openList, closedList, gScoreMap) <- get
       when (neighbor == goal) $ do
             modify (\(a,b,c) -> ([],[],Map.empty))
             liftInner $ exit ()
-      -- TODO this is always true due to above
-      if neighbor `elem` closedList
-        then modify id
-        else 
-          let Just distFromOrigin = Map.lookup current gScoreMap in
-          let tentative_score =  distFromOrigin + (heuristicDist current neighbor) in
-          if (neighbor `elem` openList) == False || tentative_score < (fromJust ( Map.lookup neighbor gScoreMap))
-            then do
-              modify (\(a,b,c) -> (a,b, Map.insert neighbor tentative_score c))
-              if (neighbor `elem` openList) == False
-                then modify (\(a,b,c) -> (neighbor:a, b, c))
-                else modify id
-            else modify id
+
+      let Just distFromOrigin = Map.lookup current gScoreMap
+      let Just neighbor_score = Map.lookup neighbor gScoreMap
+      let tentative_score     = distFromOrigin + (heuristicDist current neighbor)
+      let bool = ((neighbor `elem` openList) == False) || tentative_score < neighbor_score
+
+      when bool $ do
+        modify (\(a,b,c) -> (a,b, Map.insert neighbor tentative_score c))
+        modify (\(a,b,c) -> (neighbor:a, b, c))
      )
   (openList,_,_) <- get
   if null openList
     then do return "hi"
-    else do go
+    else do return "hi"
+    --else do go world
   where liftInner = id
 
-g = runState go startSearchState
-
-    
 generateCells :: [Cell]
 generateCells = 
     (take (size *  10) (repeat G)) ++ (take (size * 30) (repeat A)) ++ (take (size * 60) (repeat E))
     where size = n*n `quot` 100
           n = worldSize
 
+-- returns a simple map of all empties
+generateCells' :: [Cell]
+generateCells' = take size $ repeat E
+    where size = n*n 
+          n = worldSize
 
+m = main
         
 main = do
-    let cells = unsafePerformIO . shuffleIO $ generateCells
+    let cells = unsafePerformIO . shuffleIO $ generateCells'
     let world = DLS.chunksOf worldSize cells
-    return world
+    print $ runState (go [[E]]) startSearchState
+    --x <- go world startSearchState
+    return "exiting..."
 
 
 
